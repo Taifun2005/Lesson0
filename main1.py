626d598cb4756d1c7bb3976e9fd3338543f75ef5
Taifun2005 <lider2005@yandex.ru>
2024-07-31 19:43:21 +1100

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,14 @@
+# 1st program
+print((9 ** .5) * 5)
+
+# 2st program
+print( 9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+print(1234 // 10 % 100 + 5678 // 10 % 100)
+
+# 4st program
+#print(int(13.42 // 1), int(13.42 * 100 % 100), int(42.13 // 1), int(42.13 * 100 % 100))
+#print(int(13.42 // 1) == int(42.13 // 1))
+#print(int(13.42 // 1) == int(42.13 * 100 % 100))
+print(int(13.42 // 1) == int(42.13 // 1) or int(13.42 // 1) == int(42.13 * 100 % 100) )
